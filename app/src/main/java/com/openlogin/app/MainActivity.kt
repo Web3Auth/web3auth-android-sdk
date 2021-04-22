2dc0345bf29cebe9a9c19ef028b9f5cc9c15683e
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-04-22 17:21:46 +0700

    Add basic README

--- app/src/main/java/com/openlogin/app/MainActivity.kt
+++ app/src/main/java/com/openlogin/app/MainActivity.kt
@@ -1,46 +1,29 @@
 package com.openlogin.app
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.SignInButton
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.tasks.Task
+import com.openlogin.core.OpenLogin
+
 
 class MainActivity : AppCompatActivity() {
-    companion object {
-        const val SIGN_IN_WITH_GOOGLE = 9000
-    }
+    private lateinit var openlogin: OpenLogin
 
-    private lateinit var googleSignIn: GoogleSignInClient
+    private var account: String? = null
 
-    private var googleAccount: GoogleSignInAccount? = null
-
-    private fun signInWithGoogle() {
-        if (googleAccount != null) return
-        startActivityForResult(googleSignIn.signInIntent, SIGN_IN_WITH_GOOGLE)
-    }
-
-    private fun onSignedInWithGoogle(task: Task<GoogleSignInAccount>) {
-        try {
-            googleAccount = task.result
-        } catch (e: ApiException) {
-            Log.w(localClassName, "signInWithGoogle:failed code=" + e.statusCode);
+    private fun signIn() {
+        openlogin.login("google").thenApply {
+            account = it.first()
+            reRender()
         }
-        reRender()
     }
 
     private fun signOut() {
-        googleSignIn.signOut().addOnCompleteListener {
-            googleAccount = null
+        openlogin.logout().thenApply {
+            account = null
             reRender()
         }
     }
@@ -50,9 +33,9 @@ class MainActivity : AppCompatActivity() {
         val signInWithGoogleButton = findViewById<SignInButton>(R.id.signInWithGoogleButton)
         val signOutButton = findViewById<Button>(R.id.signOutButton)
 
-        val account = googleAccount
+        val account = account
         if (account != null) {
-            contentTextView.text = account.displayName
+            contentTextView.text = account
             contentTextView.visibility = View.VISIBLE
             signInWithGoogleButton.visibility = View.GONE
             signOutButton.visibility = View.VISIBLE
@@ -68,28 +51,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        googleSignIn = GoogleSignIn.getClient(
-            this,
-            GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build()
-        )
+        openlogin = OpenLogin(this)
 
         val signInWithGoogleButton = findViewById<SignInButton>(R.id.signInWithGoogleButton)
         val signInWithGoogleTextView = signInWithGoogleButton.getChildAt(0) as TextView
         signInWithGoogleTextView.text = getString(R.string.sign_in_with_google)
-        signInWithGoogleButton.setOnClickListener { signInWithGoogle() }
+        signInWithGoogleButton.setOnClickListener { signIn() }
 
         val signOutButton = findViewById<Button>(R.id.signOutButton)
         signOutButton.setOnClickListener { signOut() }
 
-        googleAccount = GoogleSignIn.getLastSignedInAccount(this)
         reRender()
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == SIGN_IN_WITH_GOOGLE) {
-            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-            onSignedInWithGoogle(task)
-        }
     }
 }
--- core/src/main/java/com/openlogin/core/OpenLogin.kt
+++ core/src/main/java/com/openlogin/core/OpenLogin.kt
@@ -1,8 +1,11 @@
 package com.openlogin.core
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import java.util.concurrent.CompletableFuture
 
-class OpenLogin : IOpenLogin {
+class OpenLogin(val context: Context) : IOpenLogin {
     override fun login(
         loginProvider: String?,
         fastLogin: Boolean,
@@ -10,6 +13,8 @@ class OpenLogin : IOpenLogin {
         appState: String?,
         extraLoginOptions: Any?
     ): CompletableFuture<Array<String>> {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://app.openlogin.com"))
+        context.startActivity(intent)
         return CompletableFuture.completedFuture(arrayOf("<private key>"))
     }
 
@@ -18,6 +23,7 @@ class OpenLogin : IOpenLogin {
         redirectUrl: String?,
         appState: String?
     ): CompletableFuture<Void> {
+        // TODO: Log out
         return CompletableFuture.completedFuture(null)
     }
 }
