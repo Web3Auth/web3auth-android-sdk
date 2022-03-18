2a6cc954c902df202036512eb284bf0584754db9
chaitanyapotti <chaitanya.potti@gmail.com>
2022-03-18 15:55:39 -0400

    Add support for ed25519 private key

--- core/src/main/java/com/web3auth/core/types/Web3AuthResponse.kt
+++ core/src/main/java/com/web3auth/core/types/Web3AuthResponse.kt
@@ -5,6 +5,7 @@ import androidx.annotation.Keep
 @Keep
 data class Web3AuthResponse(
     val privKey: String? = null,
+    val ed25519PrivKey: String? = null,
     val userInfo: UserInfo? = null,
     val error: String? = null
 )
