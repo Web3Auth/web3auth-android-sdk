b8bc8cae1b15c556e520c552627f2cbf93f6df45
chaitanyapotti <chaitanya.potti@gmail.com>
2022-08-24 19:47:03 +0800

    mark redirect uri as transient

--- core/src/main/java/com/web3auth/core/types/Web3AuthOptions.kt
+++ core/src/main/java/com/web3auth/core/types/Web3AuthOptions.kt
@@ -8,7 +8,7 @@ data class Web3AuthOptions (
     val context: Context,
     val clientId: String,
     val network: Web3Auth.Network,
-    val redirectUrl: Uri? = null,
+    @Transient val redirectUrl: Uri? = null,
     val sdkUrl: String = "https://sdk.openlogin.com",
     val whiteLabel: WhiteLabelData? = null,
     val loginConfig: HashMap<String, LoginConfigItem>? = null,
