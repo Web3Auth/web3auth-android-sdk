613805f134920e3d9606089fee089aa06e912190
Gaurav Goel <gaurav@tor.us>
2023-10-06 16:35:32 +0530

    resolved PR comments
    Signed-off-by: Gaurav Goel <gaurav@tor.us>

--- Assets/Plugins/Web3AuthSDK/Types/WhiteLabelData.cs
+++ Assets/Plugins/Web3AuthSDK/Types/WhiteLabelData.cs
@@ -1,12 +1,12 @@
 ﻿using System.Collections.Generic;
 
 public class WhiteLabelData { 
-    public string appName { get; set; }
-    public string logoLight { get; set; }
-    public string logoDark { get; set; }
-    public string defaultLanguage { get; set; } = "en";
-    public string mode { get; set; } = "light";
-    public Dictionary<string, string> theme { get; set; }
+    public string? appName { get; set; }
+    public string? logoLight { get; set; }
+    public string? logoDark { get; set; }
+    public string? defaultLanguage { get; set; } = "en";
+    public string? mode { get; set; } = "light";
+    public Dictionary<string, string>? theme { get; set; }
     public string? appUrl { get; set; }
     public bool? useLogoLoader { get; set; } = false;
 }
