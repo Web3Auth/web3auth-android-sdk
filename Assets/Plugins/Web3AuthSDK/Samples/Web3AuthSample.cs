17be1a6a54b4ac135ff858a5d0e1b62d8e377dd3
Gaurav Goel <grvgoel19@gmail.com>
2023-11-06 13:45:36 +0530

    feat: add secp256k1 curve by default
    Signed-off-by: Gaurav Goel <gaurav@tor.us>

--- Assets/Plugins/Web3AuthSDK/Samples/Web3AuthSample.cs
+++ Assets/Plugins/Web3AuthSDK/Samples/Web3AuthSample.cs
@@ -73,7 +73,7 @@ public class Web3AuthSample : MonoBehaviour
             }
             */
             clientId = "BPi5PB_UiIZ-cPz1GtV5i1I2iOSOHuimiXBI0e-Oe_u6X3oVAbCiAZOTEBtTXw4tsluTITPqA8zMsfxIKMjiqNQ",
-            buildEnv = BuildEnv.PRODUCTION,
+            buildEnv = BuildEnv.TESTING,
             redirectUrl = new Uri("torusapp://com.torus.Web3AuthUnity/auth"),
             network = Web3Auth.Network.SAPPHIRE_MAINNET,
             sessionTime = 86400
