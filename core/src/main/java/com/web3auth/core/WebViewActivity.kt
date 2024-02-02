c3f63e9ab8cfb51c7c867a50598fc9fecc22e7b1
Gaurav Goel <grvgoel19@gmail.com>
2024-02-02 15:21:14 +0530

    feat: swipeToRefresh added in WebViewActivity.kt along with theme in core library
    Signed-off-by: Gaurav Goel <gaurav@tor.us>

--- core/src/main/java/com/web3auth/core/WebViewActivity.kt
+++ core/src/main/java/com/web3auth/core/WebViewActivity.kt
@@ -2,25 +2,43 @@ package com.web3auth.core
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.ViewTreeObserver.OnScrollChangedListener
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.web3auth.core.types.WALLET_URL
 
 class WebViewActivity : AppCompatActivity() {
 
     private lateinit var webView: WebView
+    private var swipeRefreshLayout: SwipeRefreshLayout? = null
+    private var mOnScrollChangedListener: OnScrollChangedListener? = null
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_webview)
+        webView = findViewById(R.id.webView)
+        swipeRefreshLayout = findViewById(R.id.swipeRefreshLayout)
+
         val extras = intent.extras
         if (extras != null) {
             val walletUrl = extras.getString(WALLET_URL)
 
-            webView = WebView(this)
-            webView.webViewClient = WebViewClient()
+            webView.webViewClient = object : WebViewClient() {
+                override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                    if (walletUrl != null) {
+                        view?.loadUrl(walletUrl)
+                    }
+                    return false;
+                }
+
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    swipeRefreshLayout?.isRefreshing = false
+                }
+            }
 
             val webSettings: WebSettings = webView.settings
             webSettings.javaScriptEnabled = true
@@ -31,6 +49,22 @@ class WebViewActivity : AppCompatActivity() {
                 webView.loadUrl(walletUrl)
             }
         }
-        setContentView(webView)
+
+        swipeRefreshLayout?.setOnRefreshListener {
+            webView.reload()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        swipeRefreshLayout?.viewTreeObserver?.addOnScrollChangedListener(
+            OnScrollChangedListener {
+                swipeRefreshLayout?.isEnabled = webView.scrollY == 0;
+            }.also { mOnScrollChangedListener = it })
+    }
+
+    override fun onStop() {
+        super.onStop()
+        swipeRefreshLayout?.viewTreeObserver?.removeOnScrollChangedListener(mOnScrollChangedListener);
     }
 }
